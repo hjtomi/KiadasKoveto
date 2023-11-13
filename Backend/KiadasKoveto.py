e473f7952da1875f7f1c2d1d651c3217ec13bbc6
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2023-11-13 16:24:55 +0100

    flask szerver es db kezdet

--- Backend/KiadasKoveto.py
+++ Backend/KiadasKoveto.py
@@ -3,7 +3,7 @@ import json
 
 url = "https://api.mindee.net/v1/products/mindee/expense_receipts/v5/predict"
 
-#Create a json
+# Create a json
 with open("nyugta1.jpg", "rb") as myfile:
     files = {"document": myfile}
     headers = {"Authorization": "db7e7f5ffcf2b8810e115fda0b1cd1cb"}
@@ -12,15 +12,15 @@ with open("nyugta1.jpg", "rb") as myfile:
     with open("response1.json", "w") as f:
         json.dump(json.loads(response.text), f)
 
-#look for something in the text
+# look for something in the text
 with open("response1.json", "r") as i:
     data = json.load(i)
 
-#kulcsok az egyik fő kulcsban
+# kulcsok az egyik fő kulcsban
 print(data['document'])
 
-#lényeges kulcsok
+# lényeges kulcsok
 print(data['document']['inference']['pages'][0]['prediction'].keys())
 
-#aruk
+# aruk
 print(data['document']['inference']['pages'][0]['prediction']['line_items'])
