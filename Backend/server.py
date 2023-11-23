75f6ce5dc7f4a469886096b62cddd43c5c83e489
Szarazkoro <bodor.daniel3@gmail.com>
2023-11-23 16:20:12 +0100

    server hiba javitas

--- Backend/server.py
+++ Backend/server.py
@@ -22,14 +22,15 @@ class Felhasznalo(db.Model):
 
 
 class Tranzakcio(db.Model):
-    felhasznalonev = db.Column(db.String(25), primary_key=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    felhasznalonev = db.Column(db.String(25), primary_key=False, unique=False)
     tranzakcio_id = db.Column(db.Integer, unique=False, nullable=False)
     tipus = db.Column(db.String(50), unique=False, nullable=False)
     ertek = db.Column(db.Integer)
     datum = db.Column(db.String(25))
     bolti_aru_nev = db.Column(db.String(50))
     sajat_aru_nev = db.Column(db.String(50))
-    #bolt = db.Column(db.String(50))
+    bolt = db.Column(db.String(50))
 
 
 with app.app_context():
@@ -43,12 +44,12 @@ def hello_world():
 
 @app.route("/nyugtas_kiadas_felvetel")
 def nyugtas_kiadas():
-    felhasznalonev = "Proba"
+    felhasznalonev = "Proba1"
     print(bool(0))
     # with open('nyugta3.jpg', 'rb') as file:
     #     adatok = nyk.nyugta_adatfeldolgozas(file)
     nyugta_adatok = {
-    'bolt_nev': None,
+    'bolt_nev': "Spar",
     'datum': None,
     'bolti_termek_nevek': ['B00 db DORMI EPRES 30G 149 Ft/db', 'A00 CSIRKEMELLF. SZCS', 'A00 CSIRKEMELLE SZCS', 'CO0 GYE.ZABFAL.CS02244G', 'CO0 MILKA.MAZS.MOGY 100G', 'BO0 db PANTASTICO RETRO 489 Ft/db PUF', 'CO0 BEVASARLO TASKA', 'COO SZT.KIRALYI BABA 1L', 'COO AN BACON 2X200G', 'CO0 KOCKAZOTT BACON 500G', 'AOO AN SERTES-MARHA MIX', 'CO0 PF. KACSAZSIR 200G', 'CO0 THYMOS CHILT OROLT25', 'CO0 HR SZEGFUSZEG.10 G.', 'COO KEREKANYA KERESZTK', 'COO HOBBY-SZALAG, EZUST.', 'COO MC PROF400G SZARNY', 'db 479 Ft/db', 'COO MORANDO PROFESSIONAL', 'BO0 CHED.LAPKA SAJT 150G'],
     'bolti_termek_arak': [298.0, 220.0, 436.0, 529.0, 549.0, 978.0, 299.0, 169.0, 899.0, 999.0, 385.0, 199.0, 499.0, 249.0, 339.0, 699.0, 479.0, 958.0, 479.0, 659.0]
@@ -80,14 +81,17 @@ def nyugtas_kiadas():
     for (sajat_termek_ar, bolti_termek_nev, sajat_termek_nev) in zip(kesz_adatok['sajat_termek_arak'],
                                                                      kesz_adatok['bolti_termek_nevek'],
                                                                      kesz_adatok['sajat_termek_nevek']):
+        print(tranzakciok)
         uj_tranzakcio = Tranzakcio(
+            id=tranzakciok[-1].id+1,
             felhasznalonev="Proba",
             tranzakcio_id=0,
             tipus='nyugtas_kiadas',
             ertek=sajat_termek_ar,
             datum=kesz_adatok['datum'],
             bolti_aru_nev=bolti_termek_nev,
-            sajat_aru_nev=sajat_termek_nev
+            sajat_aru_nev=sajat_termek_nev,
+            bolt="Spar"
         )
         db.session.add(uj_tranzakcio)
         db.session.commit()
@@ -96,4 +100,4 @@ def nyugtas_kiadas():
 
 if __name__ == '__main__':
     print(len("a4232c55ba718564b614564076384eaf0600b537bb1ef30b9b92f8d7f806804a"))
-    app.run(debug=True)
+    app.run(debug=False)
