8fab09c8dbf5864b21bad8965a1291eb17fc204a
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2023-11-25 16:25:58 +0100

    server.py nyugtas kiadas url tisztitas kesz

--- Backend/nyugtas_kiadas.py
+++ Backend/nyugtas_kiadas.py
@@ -1,35 +1,73 @@
 # Nyugtas kiadas felvetel
 
-import io
 import requests
 from Adatbazis import Felhasznalo, Tranzakcio, adatok_hozzadasa
 
 API_KEY = "d2692ea9750aa3db29e8489cdd9b97f0"
 
+debug_nyers_nyugta_adatok = {
+        'bolt_nev': "Spar",
+        'datum': None,
+        'bolti_termek_nevek': ['Fanta 0,5L', 'B00 db DORMI EPRES 30G 149 Ft/db', 'A00 CSIRKEMELLF. SZCS',
+                               'A00 CSIRKEMELLE SZCS', 'CO0 GYE.ZABFAL.CS02244G', 'CO0 MILKA.MAZS.MOGY 100G',
+                               'BO0 db PANTASTICO RETRO 489 Ft/db PUF', 'CO0 BEVASARLO TASKA',
+                               'COO SZT.KIRALYI BABA 1L', 'COO AN BACON 2X200G', 'CO0 KOCKAZOTT BACON 500G',
+                               'AOO AN SERTES-MARHA MIX', 'CO0 PF. KACSAZSIR 200G', 'CO0 THYMOS CHILT OROLT25',
+                               'CO0 HR SZEGFUSZEG.10 G.', 'COO KEREKANYA KERESZTK', 'COO HOBBY-SZALAG, EZUST.',
+                               'COO MC PROF400G SZARNY', 'db 479 Ft/db', 'COO MORANDO PROFESSIONAL',
+                               'BO0 CHED.LAPKA SAJT 150G'],
+        'bolti_termek_arak': [399.0, 298.0, 220.0, 436.0, 529.0, 549.0, 978.0, 299.0, 169.0, 899.0, 999.0, 385.0, 199.0,
+                              499.0, 249.0, 339.0, 699.0, 479.0, 958.0, 479.0, 659.0]
+    }
+
 
 class NyugtasKiadas:
-    def __init__(self, photo=None, nyers_nyugta_adatok=None):
-        # nyers_nyugta_adatok = self.nyugta_adatfeldolgozas(photo)
-        kesz_adatok = self.nyugtas_kiadas_felvetel(nyers_nyugta_adatok)
+    def __init__(self, photo=None, debug=False):
+        if debug:
+            kesz_adatok = self.nyugtas_kiadas_felvetel(debug_nyers_nyugta_adatok)
 
-        uj_elemek = []
-        for (sajat_termek_ar, bolti_termek_nev, sajat_termek_nev) in zip(kesz_adatok['sajat_termek_arak'],
-                                                                         kesz_adatok['bolti_termek_nevek'],
-                                                                         kesz_adatok['sajat_termek_nevek']):
-            uj_elemek.append(
-                Tranzakcio(
-                    felhasznalonev="Proba",
-                    tranzakcio_id=0,
-                    tipus='nyugtas_kiadas',
-                    ertek=sajat_termek_ar,
-                    datum=kesz_adatok['datum'],
-                    bolti_aru_nev=bolti_termek_nev,
-                    sajat_aru_nev=sajat_termek_nev,
-                    bolt=kesz_adatok['bolt_nev']
+            uj_elemek = []
+            for (sajat_termek_ar, bolti_termek_nev, sajat_termek_nev) in zip(kesz_adatok['sajat_termek_arak'],
+                                                                             kesz_adatok['bolti_termek_nevek'],
+                                                                             kesz_adatok['sajat_termek_nevek']):
+                uj_elemek.append(
+                    Tranzakcio(
+                        felhasznalonev="Proba",
+                        tranzakcio_id=0,
+                        tipus='nyugtas_kiadas',
+                        ertek=sajat_termek_ar,
+                        datum=kesz_adatok['datum'],
+                        bolti_aru_nev=bolti_termek_nev,
+                        sajat_aru_nev=sajat_termek_nev,
+                        bolt=kesz_adatok['bolt_nev']
+                    )
                 )
-            )
 
-        adatok_hozzadasa(uj_elemek)
+            adatok_hozzadasa(uj_elemek)
+
+        else:
+
+            nyers_nyugta_adatok = self.nyugta_adatfeldolgozas(photo)
+            kesz_adatok = self.nyugtas_kiadas_felvetel(nyers_nyugta_adatok)
+
+            uj_elemek = []
+            for (sajat_termek_ar, bolti_termek_nev, sajat_termek_nev) in zip(kesz_adatok['sajat_termek_arak'],
+                                                                             kesz_adatok['bolti_termek_nevek'],
+                                                                             kesz_adatok['sajat_termek_nevek']):
+                uj_elemek.append(
+                    Tranzakcio(
+                        felhasznalonev="Proba",
+                        tranzakcio_id=0,
+                        tipus='nyugtas_kiadas',
+                        ertek=sajat_termek_ar,
+                        datum=kesz_adatok['datum'],
+                        bolti_aru_nev=bolti_termek_nev,
+                        sajat_aru_nev=sajat_termek_nev,
+                        bolt=kesz_adatok['bolt_nev']
+                    )
+                )
+
+            adatok_hozzadasa(uj_elemek)
 
     @staticmethod
     def nyugta_adatfeldolgozas(photo) -> dict:
--- Backend/server.py
+++ Backend/server.py
@@ -12,21 +12,14 @@ Adatbazis.db_creater(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Udv a KiadasKoveto applikacioban</p>"
 
 
 @app.route("/nyugtas_kiadas_felvetel")
 def nyugtas_kiadas():
-    nyers_nyugta_adatok = {
-    'bolt_nev': "Spar",
-    'datum': None,
-    'bolti_termek_nevek': ['Fanta 0,5L', 'B00 db DORMI EPRES 30G 149 Ft/db', 'A00 CSIRKEMELLF. SZCS', 'A00 CSIRKEMELLE SZCS', 'CO0 GYE.ZABFAL.CS02244G', 'CO0 MILKA.MAZS.MOGY 100G', 'BO0 db PANTASTICO RETRO 489 Ft/db PUF', 'CO0 BEVASARLO TASKA', 'COO SZT.KIRALYI BABA 1L', 'COO AN BACON 2X200G', 'CO0 KOCKAZOTT BACON 500G', 'AOO AN SERTES-MARHA MIX', 'CO0 PF. KACSAZSIR 200G', 'CO0 THYMOS CHILT OROLT25', 'CO0 HR SZEGFUSZEG.10 G.', 'COO KEREKANYA KERESZTK', 'COO HOBBY-SZALAG, EZUST.', 'COO MC PROF400G SZARNY', 'db 479 Ft/db', 'COO MORANDO PROFESSIONAL', 'BO0 CHED.LAPKA SAJT 150G'],
-    'bolti_termek_arak': [399.0, 298.0, 220.0, 436.0, 529.0, 549.0, 978.0, 299.0, 169.0, 899.0, 999.0, 385.0, 199.0, 499.0, 249.0, 339.0, 699.0, 479.0, 958.0, 479.0, 659.0]
-}
-    nyk.NyugtasKiadas(nyers_nyugta_adatok=nyers_nyugta_adatok)
+    nyk.NyugtasKiadas(debug=True)
     return "<p>Nyugtas kiadas felveve</p>"
 
 
 if __name__ == '__main__':
-    print(len("a4232c55ba718564b614564076384eaf0600b537bb1ef30b9b92f8d7f806804a"))
     app.run(debug=False)
