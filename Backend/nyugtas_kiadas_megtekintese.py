6d28f6871ec75e212bd497a67c02dac30d755106
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2023-11-25 18:21:03 +0100

    nyugta megtekintese tranz_id alapjan kesz

--- Backend/nyugtas_kiadas_megtekintese.py
+++ Backend/nyugtas_kiadas_megtekintese.py
@@ -5,6 +5,9 @@ class NyugtaMegtekintese:
     def __init__(self, app, tranzakcio_id):
         adatbazis_tranzakcio_adatok = Adatbazis.adatok_lekerese(app=app, tranzakcio=True)
         megfelelo_tranzakcio_adatok = list(filter(lambda x: x.tranzakcio_id == tranzakcio_id, adatbazis_tranzakcio_adatok))
+
+        print(megfelelo_tranzakcio_adatok[0].bolt)
+        print(megfelelo_tranzakcio_adatok[0].datum)
         for megfelelo_tranzakcio_adat in megfelelo_tranzakcio_adatok:
-            print(megfelelo_tranzakcio_adat.sajat_aru_nev)
+            print(f"{megfelelo_tranzakcio_adat.sajat_aru_nev} --- {megfelelo_tranzakcio_adat.ertek}Ft --- {megfelelo_tranzakcio_adat.tipus}")
 
