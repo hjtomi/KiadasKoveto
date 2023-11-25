379932836bd302a6f91e264611b01a665e22274a
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2023-11-25 18:10:32 +0100

    Nyugtas kiadas megtekintes tranzakcio_id altal

--- Backend/nyugtas_kiadas_megtekintese.py
+++ Backend/nyugtas_kiadas_megtekintese.py
@@ -3,6 +3,8 @@ import Adatbazis
 
 class NyugtaMegtekintese:
     def __init__(self, app, tranzakcio_id):
-        tranzakcio_adatok = Adatbazis.adatok_lekerese(app=app, tranzakcio=True)
-        print(tranzakcio_adatok)
+        adatbazis_tranzakcio_adatok = Adatbazis.adatok_lekerese(app=app, tranzakcio=True)
+        megfelelo_tranzakcio_adatok = list(filter(lambda x: x.tranzakcio_id == tranzakcio_id, adatbazis_tranzakcio_adatok))
+        for megfelelo_tranzakcio_adat in megfelelo_tranzakcio_adatok:
+            print(megfelelo_tranzakcio_adat.sajat_aru_nev)
 
